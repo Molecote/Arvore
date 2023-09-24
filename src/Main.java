7f1da500d348d89e7f2cc2be90da26969f65a5cb
Molecote <gabrielmolec@outlook.com>
2023-09-24 15:39:09 -0300

    Ínicio dos menus do código main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,46 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-        ArvoreBusca arvoreBusca = new ArvoreBusca();
+        System.out.println("Bem vindo ao trabalho de BST dos estudantes: Gabriel Molec; Gustavo Luiz e João Gabriel.");
+        Scanner teclado = new Scanner(System.in);
+        boolean loop = true;
+        while (loop == true){
+            System.out.println("Digite uma das seguintes opções:\n"
+                    + "1. Inserção;\n"
+                    + "2. Busca;\n"
+                    + "3. Remoção;\n"
+                    + "4. Impressão;\n"
+                    + "5. Sair.\n");
+            int op = Integer.parseInt(teclado.nextLine());
+            switch (op){
+                case 1:
+                    ArrayList<Integer> modulos = new ArrayList<Integer>();
+                    System.out.println("Digite a quantidade de nós a serem inseridos: ");
+                    int n = Integer.parseInt(teclado.nextLine());
+                    for (int i = 1; i <= n; i++) {
+                        System.out.println("Digite o valor do " + i + "° nó.");
+                        int no = Integer.parseInt(teclado.nextLine());
+                        modulos.add(no);}
+                    for (int i =0; i <= modulos.size(); i++){
+                    }
+
+                    // função de inserção.
+
+                    break;
+                case 2: // função de Busca.
+                    break;
+                case 3: // função de Remoção.
+                    break;
+                case 4: // função de Impressão.
+                    break;
+                case 5:
+                    System.out.println("Saindo...");
+                    loop = false;
+                    break;
+            }
+        }
+       /* ArvoreBusca arvoreBusca = new ArvoreBusca();
         ArvoreBusca.No no = new ArvoreBusca.No(50);
         ArvoreBusca.No no2 = new ArvoreBusca.No(25);
         ArvoreBusca.No no3 = new ArvoreBusca.No(22);
@@ -40,5 +80,6 @@ public class Main {
         arvoreBusca.excluir(arvoreBusca.raiz,55);
         arvoreBusca.imprimir();
         System.out.println("=============================");
+    }*/
     }
 }
